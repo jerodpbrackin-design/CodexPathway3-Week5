3ff71362bc40a565649e0f26898428f8fa1041b8
JerodBMC <jerod@bettermindcare.com>
2026-03-09 10:23:03 -0500

    final commit

--- src/pages/ProductCard.jsx
+++ src/pages/ProductCard.jsx
@@ -1,22 +1,26 @@
 const ProductCard = ({ img_url, price, name, desc }) => {
   return (
     <>
-      <div className="flex flex-col items-center justify-center bg-white w-80 h-auto rounded-xl shadow-md m-4">
-        <div className="flex w-full items-center justify-center p-4 pb-0">
+      <div className="flex flex-col items-center justify-center bg-white w-80 rounded-xl shadow-md m-4">
+        <div className="flex flex-inline w-full items-center justify-center p-4 pb-0">
           <img
             src={img_url}
             alt="Product Image"
             className="h-48 w-full object-cover rounded-xl shadow-md"
           />
         </div>
-        <div className="flex flex-col items-center p-4 w-full">
+        <div className="flex flex-col flex-grow justify-center items-center p-4 w-full">
           <h1 className="text-3xl text-center font-bold text-slate-800 mt-1">
             {name}
           </h1>
-          <p className="text-center text-slate-500 my-4">{desc}</p>
-          <p className="rounded-xl text-center bg-slate-600 px-6 py-2 text-white mt-3 inline-block font-semibold">
-            {price}
-          </p>
+          <p className="text-center text-slate-500">{desc}</p>
+        </div>
+        <div className="flex items-center justify-center p-4">
+          <div className=" w-full ">
+            <p className=" rounded-xl text-center bg-slate-600 px-6 py-2 text-white font-semibold">
+              {price}
+            </p>
+          </div>
         </div>
         <div className="flex flex-col mt-auto mb-6 w-full px-6">
           <button className="w-full bg-slate-900 text-white py-3 rounded-xl hover:bg-slate-700 transition font-bold">
